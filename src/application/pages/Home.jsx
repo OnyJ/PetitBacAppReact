393997c72ca0d1706dcd10989d84e21590b96918
OnyJ <julien.josselin75@hotmail.fr>
2020-06-22 20:12:28 +0200

    began Home.jsx styling and conditionnal rendering

--- src/application/pages/Home.jsx
+++ src/application/pages/Home.jsx
@@ -1,38 +1,80 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { CreateGame } from "../../game/CreateGame/pages/CreateGame";
-import { Link } from "react-router-dom";
-import CurrentGame from '../../game/CurrentGame/pages/CurrentGame'
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import CurrentGame from "../../game/CurrentGame/pages/CurrentGame";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Home = () => {
   const currentUser = useSelector((state) => state.auth.currentUser);
-  const [gameIsReady, setGameIsReady] = useState()
-  const [gameId, setGameId] = useState('')
+  const [gameIsReady, setGameIsReady] = useState();
+  const [gameId, setGameId] = useState("");
+  const [launchCreateGame, setLaunchCreateGame] = useState(false);
   // GetProfile(currentUser.id);
-  console.log(gameIsReady)
-  console.log(gameId)
+
+  const loggedOutDisplay = () => {
+    const emojiShocked = "\u{1f628} ";
+    const emojiFinger = "\u{1f449} ";
+    return (
+      <>
+        <center>
+          <p class="h5 mb-5">"Un Jeu du Petit Bac sur tous vos appareils !"</p>
+          <p class="h3 mb-4">
+            Vous n'êtes pas encore connecté au jeu {emojiShocked}
+          </p>
+          <div>
+            {emojiFinger}
+            <Link to="/login">
+              <button class="btn btn-warning btn-lg">Login</button>
+            </Link>
+          </div>
+          <br />
+          <div>
+            {emojiFinger}
+            <Link to="/signup">
+              <button class="btn btn-warning btn-lg">Signup</button>
+            </Link>
+          </div>
+        </center>
+      </>
+    );
+  };
+
+  const loggedInDisplay = () => {
+    console.log(gameIsReady);
+    console.log(gameId);
+
+    return (
+      <div>
+        <h1>Logged in home</h1>
+        <p>Home Logo</p>
+        <p>User pseudo</p>
+
+        <button
+          onClick={() => setLaunchCreateGame(true)}
+          class="btn btn-warning btn-lg"
+        >
+          Créer une Partie
+        </button>
+        {launchCreateGame && (
+          <CreateGame
+            isGameReady={(isReady) => setGameIsReady(isReady)}
+            gameIdForHome={(gameId) => setGameId(gameId)}
+          />
+        )}
+        {gameIsReady == true && <CurrentGame gameId={gameId} />}
+      </div>
+    );
+  };
 
   return (
     <>
-      {currentUser == null && <h1> HOME LALALA</h1>}
-      {currentUser && (
-        <div>
-          <h1> HOME LALALA</h1>
-        </div>
-      )}
-      <CreateGame isGameReady={isReady => setGameIsReady(isReady)} gameIdForHome={gameId => setGameId(gameId)}/>
+      <section class="container">
+        {currentUser == null && <div>{loggedOutDisplay()}</div>}
+        {currentUser && <div>{loggedInDisplay()}</div>}
+      </section>
 
-      {gameIsReady == true &&
-      <div>
-      <button>
-      <CurrentGame gameId={gameId}/>
-    </button>
-    </div>
-      
-      }
-
-     
+      {/* {gameIsReady == true && } */}
     </>
   );
 };
