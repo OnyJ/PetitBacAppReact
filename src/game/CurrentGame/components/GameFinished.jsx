1a0cee9ed197d0d25fad49efd3fd23462d22d15b
AugustinBoju <bojuaugustin@gmail.com>
2020-06-24 13:56:42 +0200

    front gamefinished/scores/play again

--- src/game/CurrentGame/components/GameFinished.jsx
+++ src/game/CurrentGame/components/GameFinished.jsx
@@ -6,10 +6,11 @@ import {Link} from 'react-router-dom'
 import GameScore from './GameScore';
 import useSelection from 'antd/lib/table/hooks/useSelection';
 import { useSelector } from 'react-redux';
+import { Button, Form } from 'react-bootstrap';
 
 
 const GameFinished = ({data}) => {
-  
+
   const finalScore = useSelector(state => state.score.score)
 
   const [score, setScore] = useState(data)
@@ -22,18 +23,24 @@ const GameFinished = ({data}) => {
 
   return(
     <>
-      
-        {!goToScore &&
-        <div>
-          <p> le score est de : {score}</p>
-          <Link to="/create_game"><button>Rejouer</button></Link>
-          <button onClick={() => setGoToScore(true)}>Voir les scores finaux</button>
-          </div>
-        }
-        {goToScore &&
-          <GameScore score={finalScore}></GameScore>
-        }
-       
+      <div className="container">
+          {!goToScore &&
+            <div className="row">
+              <div class="col">
+                <p> Votre score est de : {score} points</p>
+              </div>
+              <div class="col order-1">
+                <Link to="/create_game"><Button variant="btn btn-md btn-warning">Rejouer</Button></Link>
+              </div>
+              <div class="col order-2">
+                <Button variant="btn btn-md btn-warning" onClick={() => setGoToScore(true)}>Voir les scores finaux</Button>
+              </div>
+            </div>
+          }
+          {goToScore &&
+            <GameScore score={finalScore}></GameScore>
+          }
+        </div>
     </>
   )
 }
