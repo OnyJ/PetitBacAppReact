46c099aff395458a86fa0f2cc83dad43f94a7725
OnyJ <julien.josselin75@hotmail.fr>
2020-06-24 12:33:57 +0200

    conditionnal Navbar rendering - dropdown display

--- src/application/components/Navbar.jsx
+++ src/application/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Button } from "antd";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchUserLogout } from "../../authentication/fetchUserAuth";
 import { logoutSuccess } from "../../authentication/redux/userAuthActions";
 
 const Navbar = () => {
+  const currentUser = useSelector((state) => state.auth.currentUser);
   const dispatch = useDispatch();
 
   const logout = () => {
     fetchUserLogout();
     dispatch(logoutSuccess());
-    dispatch({type: 'RESET'})
-
+    dispatch({ type: "RESET" });
   };
   return (
     <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
@@ -30,29 +30,34 @@ const Navbar = () => {
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-        <div class="nav-item dropdown my-2 my-lg-0">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdownMenuLink"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            Compte
-          </a>
-          <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">
-              Profil
+      {currentUser && (
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
+          <div class="nav-item dropdown my-2 my-lg-0">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Compte
             </a>
-            <a class="dropdown-item" onClick={logout} href="#">
-              Deconnexion
-            </a>
+            <div
+              class="dropdown-menu "
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <a class="dropdown-item" href="#">
+                Profil
+              </a>
+              <a class="dropdown-item" onClick={logout} href="#">
+                Deconnexion
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </nav>
   );
 };
