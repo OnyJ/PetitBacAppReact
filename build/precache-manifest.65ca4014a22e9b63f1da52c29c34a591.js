879fd6eee3fc25e8a7b7a68fb5bbafaaa7179694
OnyJ <julien.josselin75@hotmail.fr>
2020-06-26 13:45:37 +0200

    remove /build from .gitignore

---
+++ build/precache-manifest.65ca4014a22e9b63f1da52c29c34a591.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7da16fbaa8def08580d0974908cb14a",
+    "url": "/PetitBacAppReact/index.html"
+  },
+  {
+    "revision": "562580c6537dc1dc0791",
+    "url": "/PetitBacAppReact/static/css/2.ffc11737.chunk.css"
+  },
+  {
+    "revision": "ac084120b5aab46e4999",
+    "url": "/PetitBacAppReact/static/css/main.20b24de0.chunk.css"
+  },
+  {
+    "revision": "562580c6537dc1dc0791",
+    "url": "/PetitBacAppReact/static/js/2.b58c7275.chunk.js"
+  },
+  {
+    "revision": "b26c984da016d0979e2f3640720ac1f4",
+    "url": "/PetitBacAppReact/static/js/2.b58c7275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac084120b5aab46e4999",
+    "url": "/PetitBacAppReact/static/js/main.5276cf2e.chunk.js"
+  },
+  {
+    "revision": "c99c278fd8ebba6d54bf",
+    "url": "/PetitBacAppReact/static/js/runtime-main.a8b85893.js"
+  },
+  {
+    "revision": "76c6cf5216a9226338977db2991ef6f6",
+    "url": "/PetitBacAppReact/static/media/creategame-palette.76c6cf52.png"
+  },
+  {
+    "revision": "d8a0b681ee4941ca28329f9140c0769f",
+    "url": "/PetitBacAppReact/static/media/friends.d8a0b681.png"
+  },
+  {
+    "revision": "5d0ae89add185a32e65e8da2a7e5dac3",
+    "url": "/PetitBacAppReact/static/media/history-parchment.5d0ae89a.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/PetitBacAppReact/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "129fb148a65e64b80322628e27505e70",
+    "url": "/PetitBacAppReact/static/media/waiting-sofa.129fb148.png"
+  }
+]);
