b2d705ba5e6f5847d19e63b9cf0897a76dd26bd8
Josselin Julien <44381592+OnyJ@users.noreply.github.com>
2020-06-26 20:39:31 +0200

    Update Friends.jsx

--- src/authentication/pages/Friends.jsx
+++ src/authentication/pages/Friends.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "../../App.scss";
 import Cookies from "js-cookie";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 // Assets
@@ -12,62 +12,63 @@ import imgGame from "../../application/assets/images/vr-gaming.png";
 import imgMedal3 from "../../application/assets/images/medal3.png";
 import imgMedal4 from "../../application/assets/images/medal4.png";
 
-
 const Friends = () => {
   const [launchCreateGame, setLaunchCreateGame] = useState(false);
-  const currentUser = useSelector(state => state.auth.currentUser)
-  const finalScore = useSelector(state => state.score.score)
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const finalScore = useSelector((state) => state.score.score);
 
-
-  return ( <>
+  return (
+    <>
       <div class="container card border-primary">
-          <div className="create-game pt-2">
-            <div className="title-div bg-secondary card mb-3">
-              <div className="pt-3"></div>
+        <div className="create-game pt-2 mb-5">
+          <div className="title-div bg-secondary card mb-3">
+            <div className="pt-3"></div>
             <div className="row">
-            <img
-                  src={imgFriends}
-                  alt="create_game_logo"
-                  width="50px"
-                  height="50px"
-                  className="ml-4 mt-1"/>
-            <h1 className="ml-3 text-light h2">
-                    Amis <br />
-            </h1>
-            <div className="container md-4">
-              <div className="row">
+              <img
+                src={imgFriends}
+                alt="create_game_logo"
+                width="50px"
+                height="50px"
+                className="ml-4 mt-1"
+              />
+              <h1 className="ml-3 text-light h2">
+                Amis <br />
+              </h1>
+            </div>
+            <div className="md-4">
+              <div className="row  mt-5">
                 <div className="col">
-              <p><br />Mes amis :</p>
-              </div>
-              <div className="row">
-                <div class="col order-1">
-                  <p><br />(Amis n'1 ...etc / update avec friendship)
+                  <p>
                     <br />
-                  <img
+                    Mes amis :
+                  </p>
+                </div>
+                <div className="row">
+                  <div class="col order-1">
+                    <img
                       src={imgFriends2}
                       alt="create_game_logo"
                       width="50px"
                       height="50px"
-                      className="ml-4 mt-1"/>
-                  </p>
-                  <br />
+                      className="ml-4 mt-1"
+                    />
+                    <br />
                   </div>
                 </div>
               </div>
-              <div>
-                  <Link
-                    to="/"
-                    className="btn btn-warning btn-lg text-dark"
-                    >
-                      JOUER
-                  </Link>
-                </div>
             </div>
           </div>
         </div>
+        <center className="w-100 mt-5">
+          <p className="h2">Vous n'avez pas d'amis pour le moment :)</p>
+        </center>
+        <div>
+          <Link to="/" className="btn btn-warning btn-lg text-dark">
+            JOUER
+          </Link>
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
