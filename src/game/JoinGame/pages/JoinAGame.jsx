ad5bd108edef432fe86be1943a43c28982e8f5e9
OnyJ <julien.josselin75@hotmail.fr>
2020-06-29 11:01:56 +0200

    update JoinAGame front and submission

--- src/game/JoinGame/pages/JoinAGame.jsx
+++ src/game/JoinGame/pages/JoinAGame.jsx
@@ -1,25 +1,40 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const JoinAGame = () => {
-  const [gameId, setGameId] = useState('')
+  const [gameId, setGameId] = useState("");
 
   const game = {
-    pathname: '/waiting_room',
-    testId: gameId
-  }
+    pathname: "/waiting_room",
+    testId: gameId,
+  };
 
-  return(
+  return (
     <>
-      <div className='container text-center' >
-        <div style={{paddingTop : '300px'}}>
-          <input type="number" value={gameId} required onChange={(e) => setGameId(e.target.value)}>
-          </input>
-          <Link to={game}><button className='btn btn-warning btn-lg text-dark' >Rejoindre la partie</button></Link>
-          </div>
+      <div className="container text-center">
+        <div className="join-a-game h-75">
+          <center className="">
+            <form className="card border-secondary p-4">
+              <input
+                type="number"
+                value={gameId}
+                required
+                onChange={(e) => setGameId(e.target.value)}
+              ></input>
+              <Link to={game}>
+                <button
+                  type="submit"
+                  className="btn btn-warning btn-lg text-dark"
+                >
+                  Rejoindre la partie
+                </button>
+              </Link>
+            </form>
+          </center>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default JoinAGame;
