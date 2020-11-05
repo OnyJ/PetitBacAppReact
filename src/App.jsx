b3fde2f3c4b88eba3e517077fdce76f4d90819df
OnyJ <julien.josselin75@hotmail.fr>
2020-11-05 15:54:12 +0100

    fix url problem, npm run deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button } from 'antd';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "antd";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./application/pages/Home";
 import Login from "./authentication/pages/Login";
 import Signup from "./authentication/pages/Signup";
@@ -15,54 +15,54 @@ import WaitingRoom from "./game/CreateGame/pages/WaitingRoom";
 import CurrentGame from "./game/CurrentGame/pages/CurrentGame";
 import JoinAGame from "./game/JoinGame/pages/JoinAGame";
 import GameMarking from "./game/CurrentGame/pages/GameMarking";
-import GameFinished from './game/CurrentGame/pages/GameFinished'
+import GameFinished from "./game/CurrentGame/pages/GameFinished";
 
 import "./App.scss";
 
 const App = () => {
   return (
     <>
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-            <Route path="/create_game">
-              <CreateGame />
-            </Route>
-            <Route path="/current_game">
-              <CurrentGame/>
-            </Route>
-            <Route path="/waiting_room">
-              <WaitingRoom/>
-            </Route>
-            <Route path="/join_a_game">
-              <JoinAGame/>
-            </Route>
-            <Route path="/game_marking">
-              <GameMarking/>
-            </Route>
-            <Route path="/profile">
-              <Profile/>
-            </Route>
-            <Route path="/history">
-              <History/>
-            </Route>
-            <Route path="/friends">
-              <Friends/>
-            </Route>
-            <Route path="/game_finished">
-              <GameFinished/>
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/create_game">
+            <CreateGame />
+          </Route>
+          <Route path="/current_game">
+            <CurrentGame />
+          </Route>
+          <Route path="/waiting_room">
+            <WaitingRoom />
+          </Route>
+          <Route path="/join_a_game">
+            <JoinAGame />
+          </Route>
+          <Route path="/game_marking">
+            <GameMarking />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/history">
+            <History />
+          </Route>
+          <Route path="/friends">
+            <Friends />
+          </Route>
+          <Route path="/game_finished">
+            <GameFinished />
+          </Route>
+          <Route exact path="/PetitBacAppReact">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 };
