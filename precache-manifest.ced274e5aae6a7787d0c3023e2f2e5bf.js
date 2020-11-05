e78d751df14ec70a984878d1caba7b79e5e943fc
OnyJ <julien.josselin75@hotmail.fr>
2020-11-05 15:55:30 +0100

    Updates

---
+++ precache-manifest.ced274e5aae6a7787d0c3023e2f2e5bf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a115212e35320f70d02e104b625640d",
+    "url": "/PetitBacAppReact/index.html"
+  },
+  {
+    "revision": "e597f9e0a525215c6336",
+    "url": "/PetitBacAppReact/static/css/2.ffc11737.chunk.css"
+  },
+  {
+    "revision": "9b0eabad5e9ff4038425",
+    "url": "/PetitBacAppReact/static/css/main.95795955.chunk.css"
+  },
+  {
+    "revision": "e597f9e0a525215c6336",
+    "url": "/PetitBacAppReact/static/js/2.72f4c8f0.chunk.js"
+  },
+  {
+    "revision": "b26c984da016d0979e2f3640720ac1f4",
+    "url": "/PetitBacAppReact/static/js/2.72f4c8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0eabad5e9ff4038425",
+    "url": "/PetitBacAppReact/static/js/main.c222f613.chunk.js"
+  },
+  {
+    "revision": "c99c278fd8ebba6d54bf",
+    "url": "/PetitBacAppReact/static/js/runtime-main.a8b85893.js"
+  },
+  {
+    "revision": "76c6cf5216a9226338977db2991ef6f6",
+    "url": "/PetitBacAppReact/static/media/creategame-palette.76c6cf52.png"
+  },
+  {
+    "revision": "d8a0b681ee4941ca28329f9140c0769f",
+    "url": "/PetitBacAppReact/static/media/friends.d8a0b681.png"
+  },
+  {
+    "revision": "4a055779ca6361a775dbcc450980c5f4",
+    "url": "/PetitBacAppReact/static/media/friendship.4a055779.png"
+  },
+  {
+    "revision": "5d0ae89add185a32e65e8da2a7e5dac3",
+    "url": "/PetitBacAppReact/static/media/history-parchment.5d0ae89a.png"
+  },
+  {
+    "revision": "54378673e697e802d850fc6598f5f7f7",
+    "url": "/PetitBacAppReact/static/media/login.54378673.png"
+  },
+  {
+    "revision": "3ed1199c56f96b9ad44c0766633018d2",
+    "url": "/PetitBacAppReact/static/media/medal.3ed1199c.png"
+  },
+  {
+    "revision": "5b0a66cc7ebbded88389c1e882e3cdc8",
+    "url": "/PetitBacAppReact/static/media/medal2.5b0a66cc.png"
+  },
+  {
+    "revision": "47523bf2d51e11678f795da4dca7ccc7",
+    "url": "/PetitBacAppReact/static/media/medal3.47523bf2.png"
+  },
+  {
+    "revision": "64221283bf571a81e8b783833dd72b28",
+    "url": "/PetitBacAppReact/static/media/medal4.64221283.png"
+  },
+  {
+    "revision": "dbd4c93ba7cf61417cf87f69adddb3a2",
+    "url": "/PetitBacAppReact/static/media/scoreboard.dbd4c93b.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/PetitBacAppReact/static/media/star.45dd5470.png"
+  },
+  {
+    "revision": "5357d12702d2f52c869be706206102a8",
+    "url": "/PetitBacAppReact/static/media/vr-gaming.5357d127.png"
+  },
+  {
+    "revision": "129fb148a65e64b80322628e27505e70",
+    "url": "/PetitBacAppReact/static/media/waiting-sofa.129fb148.png"
+  }
+]);
